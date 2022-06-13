e51d09a40a39b80c059db986e91232b26f57ec14
Donghyub Shin <go05117@naver.com>
2022-06-13 10:46:01 +0900

    Feature :: Controller 및 style 입력

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 	
+	@GetMapping("/board/write")
+	public String boardwriteForm() {
+		return "boardwrite";
+	}
 }
