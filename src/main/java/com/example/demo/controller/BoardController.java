74f205b78f092010a289f1ee675f0cacc00f5cd1
Donghyub Shin <go05117@naver.com>
2022-06-13 14:33:06 +0900

    Feature :: 상세 페이지 틀 완성

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -37,4 +37,10 @@ public class BoardController {
 		model.addAttribute("list", boardService.boardList());
 		return "boardlist";
 	}
+	
+	@GetMapping("/board/view")	// localhost:80/board/view?id=1
+	public String boardView(Model model, Integer id) {
+		model.addAttribute("board", boardService.boardView(id));
+		return "boardview";
+	}
 }
