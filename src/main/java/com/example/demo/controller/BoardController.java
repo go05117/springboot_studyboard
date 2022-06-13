12caddcf51db6c8c714bc78db97e6774590cb67f
Donghyub Shin <go05117@naver.com>
2022-06-13 11:31:53 +0900

    Feature :: 웹 페이지 입력한 데이터 DB 저장 구현

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,13 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Board;
+import com.example.demo.service.BoardService;
+
+
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping("/board/write")
 	public String boardwriteForm() {
 		return "boardwrite";
+	}
+	
+	@PostMapping("/board/writepro")
+	public String boardWritePro(Board board) {
+//		System.out.println("제목 : " + board.getTitle());
+//		System.out.println("내용 : " + board.getContent());
+		
+		boardService.write(board);
+		
+		return "";
 	}
 }
